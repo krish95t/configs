a5ea50feb99a1628b0626570b8c3bdea1f947462
saurabh95ch <89478462+saurabh95ch@users.noreply.github.com>
2021-10-12 00:25:41 +0530

    Create setup.sh

---
+++ scripts/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Install vim plug"
+read ans
+if [ $ans = "y" ]; then
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+fi
+
+ans="n"
+echo "Install zsh setup"
+read ans
+if [ $ans = "y" ]; then
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
