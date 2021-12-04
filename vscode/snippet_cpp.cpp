24f49c9574c369940adc13c7ff0524cce3da1a9b
Saurabh Chaudhary <89478462+monk95@users.noreply.github.com>
2021-12-04 22:38:33 +0530

    Create snippet_cpp.cpp
    C++ snippet for Competitive Programming

---
+++ vscode/snippet_cpp.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define rept(i,a,n) for(int i=a; i<(n); i++)
+#define arlen(ar) sizeof(ar)/sizeof(ar[0])
+#define all(itr) itr.begin(),itr.end()
+#define rall(itr) itr.end(),itr.begin()
+#define db1(x) cout<<#x<<"="<<x<<'\n'
+#define db2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<'\n'
+#define db3(x,y,z) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<","<<#z<<"="<<z<<'\n'
+
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+typedef unsigned int uint;
+typedef long long int int64;
+typedef unsigned long long int uint64;
+
+
+
+int32_t main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+    #endif
+
+    
+    return 0;
+}
